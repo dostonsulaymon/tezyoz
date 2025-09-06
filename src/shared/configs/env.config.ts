bb7b5d00613dbc454c8cc533c5a57d5a14672403
dostonsulaymon <dostonqosimiy19@gmail.com>
2025-09-06 09:16:47 +0500

    fix: update the project

--- src/shared/configs/env.config.ts
+++ src/shared/configs/env.config.ts
@@ -3,12 +3,7 @@ import { z } from 'zod';
 const envConfigSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
   PORT: z.coerce.number().int().positive(),
-  // REDIS_URL: z.string().url(),
-  // REDIS_PASSWORD: z.string().min(1, 'REDIS_PASSWORD must be at least 8 characters long'),
-  DATABASE_USER: z.string().min(1, 'DATABASE_USER cannot be empty'),
-  DATABASE_NAME: z.string().min(1, 'DATABASE_NAME cannot be empty'),
   DATABASE_URL: z.string().url(),
-  DATABASE_PASSWORD: z.string().min(1, 'DATABASE_PASSWORD cannot be empty'),
 });
 
 export type EnvConfig = z.infer<typeof envConfigSchema>;
