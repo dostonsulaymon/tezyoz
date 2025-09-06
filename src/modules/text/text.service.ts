b4b19a59d4d5bd2b72cbbe3608eda1a52d1a5ba4
dostonsulaymon <dostonqosimiy19@gmail.com>
2025-09-06 18:12:18 +0500

    fix: finish text creation

--- src/modules/text/text.service.ts
+++ src/modules/text/text.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTextDto } from '#/modules/text/dto/request/create-text.dto';
+import { DatabaseService } from '#/modules/database/database.service';
 
 @Injectable()
 export class TextService {
+
+  constructor(private readonly databaseService: DatabaseService) {
+  }
+
   async createOne(createTextDto: CreateTextDto) {
-    return Promise.resolve(undefined);
+    const newText = await this.databaseService.text.create({
+      data: createTextDto,
+    });
+
+    return {
+      message: 'New text created successfully.',
+      newText,
+    };
   }
 
   async createBulk(texts: CreateTextDto[]) {
-    return Promise.resolve(undefined);
+    const result = await this.databaseService.text.createMany({
+      data: texts,
+    });
+
+    return {
+      message: 'Bulk texts created successfully.',
+      count: result.count,
+    };
   }
 }
